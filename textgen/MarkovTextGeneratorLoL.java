2aaca27a8f37d6ae02028560ed7911a8804d3ac1
janebarinova <janebarinova@gmail.com>
2016-05-03 17:45:12 -0700

    Feat: LinkedList implementation and Markov text generator.

--- textgen/MarkovTextGeneratorLoL.java
+++ textgen/MarkovTextGeneratorLoL.java
@@ -5,75 +5,129 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Random;
 
-/** 
+/**
  * An implementation of the MTG interface that uses a list of lists.
- * @author UC San Diego Intermediate Programming MOOC team 
+ * 
+ * @author UC San Diego Intermediate Programming MOOC team
+ * @author Jane Bautina
  */
 public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 
 	// The list of words with their next words
-	private List<ListNode> wordList; 
-	
+	private List<ListNode> wordList;
+
 	// The starting "word"
 	private String starter;
-	
+
 	// The random number generator
 	private Random rnGenerator;
-	
-	public MarkovTextGeneratorLoL(Random generator)
-	{
+
+	public MarkovTextGeneratorLoL(Random generator) {
 		wordList = new LinkedList<ListNode>();
 		starter = "";
 		rnGenerator = generator;
 	}
-	
-	
+
 	/** Train the generator by adding the sourceText */
 	@Override
-	public void train(String sourceText)
-	{
-		// TODO: Implement this method
+	public void train(String sourceText) {
+		if (!wordList.isEmpty()) {
+			return;
+		}
+		String[] arrayOfWords = sourceText.split(" ");
+
+		starter = arrayOfWords[0];
+		String prevWord = starter;
+
+		for (int i = 1; i < arrayOfWords.length; i++) {
+			int index = containsWord(prevWord);
+			if (index == -1) {
+				ListNode ln = new ListNode(prevWord);
+				ln.addNextWord(arrayOfWords[i]);
+				wordList.add(ln);
+			} else {
+				wordList.get(index).addNextWord(arrayOfWords[i]);
+			}
+			prevWord = arrayOfWords[i];
+		}
+		// last Word
+		int index = containsWord(prevWord);
+		if (index == -1) {
+			ListNode ln = new ListNode(prevWord);
+			ln.addNextWord(starter);
+			wordList.add(ln);
+		} else {
+			wordList.get(index).addNextWord(starter);
+		}
 	}
-	
-	/** 
+
+	/**
+	 * Check if wordList contains the word
+	 * 
+	 * @param word
+	 * @return -1 if wordList doesn't contain the word index of the word in the
+	 *         list otherwise
+	 */
+	private int containsWord(String word) {
+		int index = 0;
+		for (ListNode n : wordList) {
+			if (n.getWord().equals(word)) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+
+	/**
 	 * Generate the number of words requested.
 	 */
 	@Override
 	public String generateText(int numWords) {
-	    // TODO: Implement this method
-		return null;
+		if (wordList.isEmpty()) {
+			return "";
+		}
+		if (numWords == 0) {
+			return "";
+		}
+		String currentWord = starter;
+		String outPut = "";
+		outPut += currentWord + " ";
+		int numberOfWords = 1;
+		while (numberOfWords < numWords) {
+			ListNode ln = wordList.get(containsWord(currentWord));
+			String randomWord = ln.getRandomNextWord(rnGenerator);
+			outPut += randomWord + " ";
+			currentWord = randomWord;
+			numberOfWords++;
+		}
+		return outPut;
 	}
-	
-	
+
 	// Can be helpful for debugging
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		String toReturn = "";
-		for (ListNode n : wordList)
-		{
+		for (ListNode n : wordList) {
 			toReturn += n.toString();
 		}
 		return toReturn;
 	}
-	
+
 	/** Retrain the generator from scratch on the source text */
 	@Override
-	public void retrain(String sourceText)
-	{
-		// TODO: Implement this method.
+	public void retrain(String sourceText) {
+		wordList.clear();
+		train(sourceText);
 	}
-	
-	// TODO: Add any private helper methods you need here.
-	
-	
+
 	/**
-	 * This is a minimal set of tests.  Note that it can be difficult
-	 * to test methods/classes with randomized behavior.   
+	 * This is a minimal set of tests. Note that it can be difficult to test
+	 * methods/classes with randomized behavior.
+	 * 
 	 * @param args
 	 */
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		// feed the generator a fixed random value for repeatable behavior
 		MarkovTextGeneratorLoL gen = new MarkovTextGeneratorLoL(new Random(42));
 		String textString = "Hello.  Hello there.  This is a test.  Hello there.  Hello Bob.  Test again.";
@@ -81,30 +135,22 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 		gen.train(textString);
 		System.out.println(gen);
 		System.out.println(gen.generateText(20));
-		String textString2 = "You say yes, I say no, "+
-				"You say stop, and I say go, go, go, "+
-				"Oh no. You say goodbye and I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello. "+
-				"I say high, you say low, "+
-				"You say why, and I say I don't know. "+
-				"Oh no. "+
-				"You say goodbye and I say hello, hello, hello. "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello. "+
-				"Why, why, why, why, why, why, "+
-				"Do you say goodbye. "+
-				"Oh no. "+
-				"You say goodbye and I say hello, hello, hello. "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello. "+
-				"You say yes, I say no, "+
-				"You say stop and I say go, go, go. "+
-				"Oh, oh no. "+
-				"You say goodbye and I say hello, hello, hello. "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello, hello, hello, "+
-				"I don't know why you say goodbye, I say hello, hello, hello,";
+		String textString2 = "You say yes, I say no, " + "You say stop, and I say go, go, go, "
+				+ "Oh no. You say goodbye and I say hello, hello, hello, "
+				+ "I don't know why you say goodbye, I say hello, hello, hello, "
+				+ "I don't know why you say goodbye, I say hello. " + "I say high, you say low, "
+				+ "You say why, and I say I don't know. " + "Oh no. "
+				+ "You say goodbye and I say hello, hello, hello. "
+				+ "I don't know why you say goodbye, I say hello, hello, hello, "
+				+ "I don't know why you say goodbye, I say hello. " + "Why, why, why, why, why, why, "
+				+ "Do you say goodbye. " + "Oh no. " + "You say goodbye and I say hello, hello, hello. "
+				+ "I don't know why you say goodbye, I say hello, hello, hello, "
+				+ "I don't know why you say goodbye, I say hello. " + "You say yes, I say no, "
+				+ "You say stop and I say go, go, go. " + "Oh, oh no. "
+				+ "You say goodbye and I say hello, hello, hello. "
+				+ "I don't know why you say goodbye, I say hello, hello, hello, "
+				+ "I don't know why you say goodbye, I say hello, hello, hello, "
+				+ "I don't know why you say goodbye, I say hello, hello, hello,";
 		System.out.println(textString2);
 		gen.retrain(textString2);
 		System.out.println(gen);
@@ -113,42 +159,42 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 
 }
 
-/** Links a word to the next words in the list 
- * You should use this class in your implementation. */
-class ListNode
-{
-    // The word that is linking to the next words
+/**
+ * Links a word to the next words in the list You should use this class in your
+ * implementation.
+ */
+class ListNode {
+	// The word that is linking to the next words
 	private String word;
-	
+
 	// The next words that could follow it
 	private List<String> nextWords;
-	
-	ListNode(String word)
-	{
+	private int nextWordsSize;
+
+	ListNode(String word) {
 		this.word = word;
 		nextWords = new LinkedList<String>();
+		nextWordsSize = 0;
 	}
-	
-	public String getWord()
-	{
+
+	public String getWord() {
 		return word;
 	}
 
-	public void addNextWord(String nextWord)
-	{
-		nextWords.add(nextWord);
-	}
-	
-	public String getRandomNextWord(Random generator)
-	{
-		// TODO: Implement this method
-	    // The random number generator should be passed from 
-	    // the MarkovTextGeneratorLoL class
-	    return null;
+	public int getNextWordsSize() {
+		return nextWordsSize;
 	}
 
-	public String toString()
-	{
+	public void addNextWord(String nextWord) {
+		nextWords.add(nextWord);
+		nextWordsSize++;
+	}
+
+	public String getRandomNextWord(Random generator) {
+		return nextWords.get(generator.nextInt(nextWordsSize));
+	}
+
+	public String toString() {
 		String toReturn = word + ": ";
 		for (String s : nextWords) {
 			toReturn += s + "->";
@@ -156,7 +202,5 @@ class ListNode
 		toReturn += "\n";
 		return toReturn;
 	}
-	
+
 }
-
-
--- textgen/MyLinkedList.java
+++ textgen/MyLinkedList.java
@@ -2,12 +2,15 @@ package textgen;
 
 import java.util.AbstractList;
 
+import org.omg.CORBA.Current;
 
-/** A class that implements a doubly linked list
+/**
+ * A class that implements a doubly linked list
  * 
  * @author UC San Diego Intermediate Programming MOOC team
- *
- * @param <E> The type of the elements stored in the list
+ * @author Jane Bautina
+ * @param <E>
+ *            The type of the elements stored in the list
  */
 public class MyLinkedList<E> extends AbstractList<E> {
 	LLNode<E> head;
@@ -16,73 +19,150 @@ public class MyLinkedList<E> extends AbstractList<E> {
 
 	/** Create a new empty LinkedList */
 	public MyLinkedList() {
-		// TODO: Implement this method
+		size = 0;
+		head = new LLNode<E>(null);
+		tail = new LLNode<E>(null);
+		head.next = tail;
+		tail.prev = head;
 	}
 
 	/**
 	 * Appends an element to the end of the list
-	 * @param element The element to add
+	 * 
+	 * @param element
+	 *            The element to add
 	 */
-	public boolean add(E element ) 
-	{
-		// TODO: Implement this method
-		return false;
+	public boolean add(E element) {
+		if (element == null) {
+			throw new NullPointerException("Setting null poiter element");
+		}
+		// 1. Create a new LLNode
+		LLNode newNode = new LLNode(element);
+		// 2. Link the new Node in
+		newNode.next = tail;
+		newNode.prev = newNode.next.prev;
+		newNode.prev.next = newNode;
+		newNode.next.prev = newNode;
+		// 3. Update Size
+		size++;
+		return true;
 	}
 
-	/** Get the element at position index 
-	 * @throws IndexOutOfBoundsException if the index is out of bounds. */
-	public E get(int index) 
-	{
-		// TODO: Implement this method.
-		return null;
+	/**
+	 * Get the element at position index
+	 * 
+	 * @throws IndexOutOfBoundsException
+	 *             if the index is out of bounds.
+	 */
+	public E get(int index) {
+		if (size == 0) { // empty list
+			throw new IndexOutOfBoundsException("Check out of bounds");
+		}
+		if ((index >= size) || (index < 0)) {
+			throw new IndexOutOfBoundsException("Check out of bounds");
+		}
+		LLNode current = head.next;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+		}
+		return (E) current.data;
 	}
 
 	/**
 	 * Add an element to the list at the specified index
-	 * @param The index where the element should be added
-	 * @param element The element to add
+	 * 
+	 * @param The
+	 *            index where the element should be added
+	 * @param element
+	 *            The element to add
 	 */
-	public void add(int index, E element ) 
-	{
-		// TODO: Implement this method
+	public void add(int index, E element) {
+		if (element == null) {
+			throw new NullPointerException("Setting null poiter element");
+		} else if ((index < 0) || (index > size)) {
+			throw new IndexOutOfBoundsException("Check out of bounds");
+		} else {
+			LLNode current = this.head.next;
+			for (int i = 0; i < index; i++) {
+				current = current.next;
+			}
+			LLNode newNode = new LLNode(element);
+			newNode.next = current;
+			newNode.prev = current.prev;
+			newNode.prev.next = newNode;
+			current.prev = newNode;
+			size++;
+		}
 	}
-
 
 	/** Return the size of the list */
-	public int size() 
-	{
-		// TODO: Implement this method
-		return -1;
+	public int size() {
+		return this.size;
 	}
 
-	/** Remove a node at the specified index and return its data element.
-	 * @param index The index of the element to remove
+	/**
+	 * Remove a node at the specified index and return its data element.
+	 * 
+	 * @param index
+	 *            The index of the element to remove
 	 * @return The data element removed
-	 * @throws IndexOutOfBoundsException If index is outside the bounds of the list
+	 * @throws IndexOutOfBoundsException
+	 *             If index is outside the bounds of the list
 	 * 
 	 */
-	public E remove(int index) 
-	{
-		// TODO: Implement this method
-		return null;
+	public E remove(int index) {
+		if (size == 0) { // empty list
+			throw new IndexOutOfBoundsException("Check out of bounds");
+		}
+		if ((index >= size) || (index < 0)) {
+			throw new IndexOutOfBoundsException("Check out of bounds");
+		}
+		LLNode current = head.next;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+		}
+		current.next.prev = current.prev;
+		current.prev.next = current.next;
+		current.prev = null;
+		current.next = null;
+		E currentData = (E) current.data;
+		current = null;
+		size--;
+		return currentData;
 	}
 
 	/**
 	 * Set an index position in the list to a new element
-	 * @param index The index of the element to change
-	 * @param element The new element
+	 * 
+	 * @param index
+	 *            The index of the element to change
+	 * @param element
+	 *            The new element
 	 * @return The element that was replaced
-	 * @throws IndexOutOfBoundsException if the index is out of bounds.
+	 * @throws IndexOutOfBoundsException
+	 *             if the index is out of bounds.
 	 */
-	public E set(int index, E element) 
-	{
-		// TODO: Implement this method
-		return null;
-	}   
+	public E set(int index, E element) {
+		if (element == null) {
+			throw new NullPointerException("Setting null poiter element");
+		}
+		if (size == 0) {
+			throw new IndexOutOfBoundsException("Check out of bounds");
+		}
+		if ((index >= size) || (index < 0)) {
+			throw new IndexOutOfBoundsException("Check out of bounds");
+		}
+		LLNode current = head.next;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+		}
+		E temp = (E) current.data;
+		current.data = element;
+		return temp;
+	}
 }
 
-class LLNode<E> 
-{
+class LLNode<E> {
 	LLNode<E> prev;
 	LLNode<E> next;
 	E data;
@@ -90,8 +170,7 @@ class LLNode<E>
 	// TODO: Add any other methods you think are useful here
 	// E.g. you might want to add another constructor
 
-	public LLNode(E e) 
-	{
+	public LLNode(E e) {
 		this.data = e;
 		this.prev = null;
 		this.next = null;
